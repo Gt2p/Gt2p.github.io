e0ddbd2ef12a80c517017fb50f7c8977f2341751
lucasGt2p <83628225+lucasGt2p@users.noreply.github.com>
2021-06-24 17:56:46 -0400

    Update main.js

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ const colorstapiz = [
   },{
   texture: './texturas/kvverde.jpg',
   size: [21, 21, 21],
-  shininess: 31
+  shininess: 30
   },{
         texture: './texturas/gris.jpg',
         size: [21, 21, 21],
